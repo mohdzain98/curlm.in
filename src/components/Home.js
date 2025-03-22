2d0a5bd89ceb43f15067666994b78d5ffb6b3830
Mohd Zain <zainmohd1998@gmail.com>
2025-03-23 00:19:14 +0530

    fix: on load

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 
 const Home = () => {
-  // useEffect(() => {
-  //   window.location.href = "https://curlmin.com";
-  // }, []);
+  useEffect(() => {
+    window.location.href = "https://curlmin.com";
+  }, []);
   return (
     <div className="d-flex justify-content-center align-items-center p-4">
       <span className="spinner-border"></span>
